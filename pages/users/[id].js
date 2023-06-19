c3d5e06c98be87b668939fcbd15dc2babadf852a
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-19 16:43:45 +0300

    add user name

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,12 +1,22 @@
 import { useRouter } from 'next/router';
 import styles from '../../styles/User.module.scss';
-export default function () {
+export default function User({ user }) {
     const router = useRouter();
     const { id } = router.query;
+    console.log({ user });
 
     return (
         <div className={styles.user}>
             <h1>User with id {id}</h1>
+            <div>User name - {user.name}</div>
         </div>
     );
+}
+
+export async function getServerSideProps({ params }) {
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${params.id}`
+    );
+    const user = await response.json();
+    return { props: { user } };
 }
