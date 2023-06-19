361ddb733a4738b89fed253676debe3916d7ace9
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-19 16:08:17 +0300

    add fetch for users

--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,7 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const users = () => {
-    const [users, setUsers] = useState([
-        { id: 1, name: 'Petya' },
-        { id: 2, name: 'Vanya' },
-    ]);
+const users = ({ users }) => {
     return (
         <div>
             <h1>List of users</h1>
@@ -23,3 +19,10 @@ const users = () => {
 };
 
 export default users;
+
+export async function getStaticProps(context) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+
+    return { props: { users } };
+}
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router';
-
+import styles from '../../styles/User.module.scss';
 export default function () {
     const router = useRouter();
     const { id } = router.query;
 
     return (
-        <div>
+        <div className={styles.user}>
             <h1>User with id {id}</h1>
         </div>
     );
