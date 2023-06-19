f19e2a68037e7e1cce0d7530c3f778d060c31eca
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-19 16:57:17 +0300

    add main container

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,15 @@
 import Link from 'next/link';
 import A from '../components/A';
+import Head from 'next/head';
+import MainContainer from '../components/MainContainer';
 
 const index = () => {
     return (
-        <div>
-            <div className="navbar">
-                <A href="/" text="Main" />
-                <A href="/users" text="Users" />
+        <MainContainer>
+            <div>
+                <h1>Main page</h1>
             </div>
-            <h1>Main page</h1>
-            <style jsx>
-                {`
-                    .navbar {
-                        background: orange;
-                        padding: 15px;
-                    }
-                    .link {
-                        text-decoration: none;
-                        color: white;
-                        font-size: 20px;
-                        margin: 10px;
-                    }
-                `}
-            </style>
-        </div>
+        </MainContainer>
     );
 };
 
