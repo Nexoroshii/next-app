cad238d54635c76bd26462f64361b86d7b4606dc
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-19 13:26:15 +0300

    add users list

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import Link from 'next/link';
+
 const index = () => {
     return (
         <div>
+            <div className="navbar">
+                <Link legacyBehavior href="/">
+                    <a className="link">Main</a>
+                </Link>
+                <Link legacyBehavior href="/users">
+                    <a>Users</a>
+                </Link>
+            </div>
             <h1>Main page</h1>
+            <style jsx>
+                {`
+                    .navbar {
+                        background: orange;
+                        padding: 15px;
+                    }
+                    .link {
+                        text-decoration: none;
+                        color: white;
+                        font-size: 20px;
+                        margin: 10px;
+                    }
+                `}
+            </style>
         </div>
     );
 };
--- pages/users.js
+++ pages/users.js
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+import { useState } from 'react';
+
 const users = () => {
+    const [users, setUsers] = useState([
+        { id: 1, name: 'Petya' },
+        { id: 2, name: 'Vanya' },
+    ]);
     return (
         <div>
             <h1>List of users</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        <Link legacyBehavior href={`/users/${user.id}`}>
+                            <a>{user.name}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
